c1269e30cef2ebb420a17e9c4cd90170065b22b7
Peter Bukowinski <pmbuko@gmail.com>
2014-03-18 12:30:01 -0400

    added debug output to dockutil_add

--- dockutil_add.sh
+++ dockutil_add.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# debug output
+echo "All arguments:"
+num=1
+for arg; do
+    echo "$num: $arg"
+    ((num++))
+done
+
 # check if we have the minimum number of arguments
 if [ -z "$4" ]; then
     echo "Didn't receive any paths. Exiting now."
@@ -12,7 +20,7 @@ shift 3
 # Process the remaining arguments, which should all be paths
 until [ -z "$1" ]; do
     echo "Adding $1 to dock..."
-    dockutil --add "${1}" || echo "Could not add $1 to dock."; exit 1
+    #dockutil --add "${1}" || echo "Could not add $1 to dock."; exit 1
     # Shift remaining arguments, so the next one becomes $1.
     shift || break
 done
