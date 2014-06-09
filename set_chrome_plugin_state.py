3fffd5f58590889c03f9f79b43690624ad0f25f0
Peter Bukowinski <pmbuko@gmail.com>
2014-06-09 14:53:39 -0400

    updated set_chrome_plugin_state tool

--- set_chrome_plugin_state.py
+++ set_chrome_plugin_state.py
@@ -8,7 +8,7 @@ myname = os.path.basename(__file__)
 # get list of local homes
 homes = glob.glob("/Users/*")
 
-# relative path to Chrome Default Preferences
+# relative path to Chrome Default Prefs
 chrome_prefs = "Library/Application Support/Google/Chrome/Default/Preferences"
 
 
@@ -20,11 +20,11 @@ def set_plugin_state(pref, plugin, state):
     """
     json_pref = json.loads(open(pref).read())
 
-    # get a list of plugins
-    plugin_list = json_pref["plugins"]["plugins_list"]
+    # get list of plugins
+    plugins_list = json_pref["plugins"]["plugins_list"]
 
-    # find plugins whose name matches 'SharePoint Browser Plug-in'
-    for dict in plugin_list:
+    # find plugins whose names supplied name 
+    for dict in plugins_list:
         if dict['name'] == plugin:
             dict['enabled'] = state
 
@@ -33,7 +33,7 @@ def set_plugin_state(pref, plugin, state):
     new_prefs.write(json.dumps(json_pref))
     new_prefs.close()
 
-def usage():    
+def usage(exit_code=1):    
     print """This program will disable or enable Chrome plugins by name for ALL local users
 and must therefore be run as root.
 
@@ -41,17 +41,18 @@ Use '-d|--disable' or '-e|--enable' and then supply the quoted plugin name, e.g.
 
     """ + myname + """ -d 'Shockwave Flash'
 """
-    sys.exit(1)
+    sys.exit(exit_code)
 
 
 def main(argv):
-    print len(sys.argv)
     if len(sys.argv) == 3:
+        plugin_name = ''
+        plugin_state = ''
         try:
             opts, args = getopt.getopt(argv,"hd:e:",["disable=","enable="])
         except getopt.GetoptError as err:
             print str(err)
-            usage()
+            usage(2)
         for opt, arg in opts:
             if opt == '-h':
                 usage()
@@ -61,17 +62,17 @@ def main(argv):
             elif opt in ("-e", "--enable"):
                 plugin_state = True 
                 plugin_name = arg 
-            else:
-                assert False, "I don't understand that option."
 
-            if os.geteuid() != 0:
-                exit("You need to have root privileges to run this script.\nPlease try again using 'sudo'. Exiting.")
-            for home in homes:
-                pref_file = home + '/' + chrome_prefs
+        # Test if running as root
+        if os.geteuid() != 0:
+            exit("You need root privileges to run this script.\nPlease try again using 'sudo'.")
 
-                if os.path.isfile(pref_file):
-                    shutil.copy(pref_file, pref_file + '.backup') # Make a backup copy
-                    set_plugin_state(pref_file, plugin_name, plugin_state) # run the pref modifier function
+        # Iterate through home directories, looking for Chrome Prefs file
+        for home in homes:
+            pref_file = home + '/' + chrome_prefs
+            if os.path.isfile(pref_file):
+                shutil.copy(pref_file, pref_file + '.backup') # Make a backup copy
+                set_plugin_state(pref_file, plugin_name, plugin_state) # run the pref modifier function
     
     else:
         usage()
