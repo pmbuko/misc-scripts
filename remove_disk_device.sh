8267d405f0c6ade6623863800f9b9bb27b83abd8
Peter Bukowinski <pmbuko@gmail.com>
2020-07-23 13:40:45 -0700

    add lsscsi output

--- remove_disk_device.sh
+++ remove_disk_device.sh
@@ -21,11 +21,17 @@ function check_root() {
   fi
 }
 
+function get_lsscsi() {
+  echo
+  lsscsi
+  echo
+}
+
 function check_dev_exists_and_unmounted() {
   if [[ -b /dev/$1 ]]; then
     if [[ $(grep $1 /proc/mounts) ]]; then
       echo "$1 is still mounted. Unmount it first."
-        exit 1
+      exit 1
     fi
   else
     echo "$1 is not a block device."
@@ -36,21 +42,27 @@ function check_dev_exists_and_unmounted() {
 function remove_bypath_links() {
   for link in /dev/disk/by-path/*; do
     if [[ $(readlink -f $link) =~ "$1" ]]; then
-      rm -f $link
-      echo "Removed $(ls -l $link | awk '{print $9,$10,$11}')"
+      echo "Removing $(ls -l $link | awk '{print $9,$10,$11}')"
+      rm $link
     fi
-  done	
+  done
 }
 
 function delete_block_device() {
+  echo "Deleting $1 block device"
   echo 1 >> /sys/block/$1/device/delete
 }
 
 check_root
 if [[ $# == 1 ]]; then
   check_dev_exists_and_unmounted $1
+  get_lsscsi
   remove_bypath_links $1
   delete_block_device $1
+  get_lsscsi
 else
   help
 fi
+
+ pbukowinski@ops0003.prn(ps4219)  ~/d/o/bin   remove_disk_device ⁝ ✚ ✱ 
+ ❯                                                                                                                                                      [13:39:50]
